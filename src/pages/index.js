41607acfc79f121a87c1e3ede9fbb596cc3c6c61
Jin Kim <golbin@gmail.com>
2023-05-16 02:54:35 +0900

    Initial commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,124 +1,141 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-
-const inter = Inter({ subsets: ['latin'] })
+import { Chat } from "@/components/Chat";
+import Head from "next/head";
+import { useEffect, useRef, useState } from "react";
 
 export default function Home() {
+  /*
+    메시지 목록을 저장하는 상태로, 메시지의 형태는 다음과 같음
+    { role: "system" | "user" | "assistant", content: string }
+
+    role 에 대한 상세한 내용은 다음 문서를 참고
+    https://platform.openai.com/docs/guides/chat/introduction
+
+    ex)
+    [
+      { role: "system", content: "너의 이름을 엘리엇이고, 나의 AI 친구야. 친절하고 명랑하게 대답해줘. 고민을 말하면 공감해줘. 반말로 대답해줘." },
+      { role: "assistant", content: "안녕? 나는 엘리엇이야. 오늘은 무슨 일이 있었니?" }
+      { role: "user", content: "오늘 재미난 일이 있었어! 한 번 들어볼래?" },
+      ...
+    ]
+  */
+  const [messages, setMessages] = useState([]);
+  // 메시지를 전송 중인지 여부를 저장하는 상태
+  const [loading, setLoading] = useState(false);
+
+  const messagesEndRef = useRef(null);
+
+  // 메시지 목록을 끝으로 스크롤
+  const scrollToBottom = () => {
+    messagesEndRef.current?.scrollIntoView({ behavior: "smooth" });
+  };
+
+  // 메시지를 전송하는 함수
+  const handleSend = async (message) => {
+    // message 를 받아 메시지 목록에 추가
+    // message 형태 = { role: "user", content: string }
+    // ChatInput.js 26번째 줄 참고
+    const updatedMessages = [...messages, message];
+    // console.log(updatedMessages);
+    // console.log(updatedMessages.slice(-6));
+
+    setMessages(updatedMessages);
+    // 메시지 전송 중임을 표시
+    setLoading(true);
+
+    // /api/chat 에 메시지 목록을 전송하고 응답을 받음
+    const response = await fetch("/api/chat", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        // 메시지 목록의 마지막 6개만 전송
+        messages: updatedMessages.slice(-6),
+      }),
+    });
+
+    if (!response.ok) {
+      setLoading(false);
+      throw new Error(response.statusText);
+    }
+
+    // 응답을 JSON 형태로 변환
+    // 비동기 API 를 사용하여 응답을 받기 때문에 await 사용
+    const result = await response.json();
+
+    if (!result) {
+      return;
+    }
+
+    // console.log(result);
+
+    // 로딩 상태를 해제하고, 메시지 목록에 응답을 추가
+    setLoading(false);
+    setMessages((messages) => [...messages, result]);
+  };
+
+  // 메시지 목록을 초기화하는 함수
+  // 처음 시작할 메시지를 설정
+  const handleReset = () => {
+    setMessages([
+      {
+        role: "assistant",
+        content: "안녕? 나는 엘리엇이야. 오늘은 무슨 일이 있었니?",
+      },
+    ]);
+  };
+
+  // 메시지 목록이 업데이트 될 때마다 맨 아래로 스크롤
+  useEffect(() => {
+    scrollToBottom();
+  }, [messages]);
+
+  // 컴포넌트가 처음 렌더링 될 때 메시지 목록을 초기화
+  useEffect(() => {
+    handleReset();
+  }, []);
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/pages/index.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <>
+      <Head>
+        <title>A Simple Chatbot</title>
+        <meta name="description" content="A Simple Chatbot" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <div className="flex flex-col h-screen">
+        <div className="flex h-[50px] sm:h-[60px] border-b border-neutral-300 py-2 px-2 sm:px-8 items-center justify-between">
+          <div className="font-bold text-3xl flex text-center">
+            <a
+              className="ml-2 hover:opacity-50"
+              href="https://code-scaffold.vercel.app"
+            >
+              A Simple Chatbot
+            </a>
+          </div>
         </div>
+
+        <div className="flex-1 overflow-auto sm:px-10 pb-4 sm:pb-10">
+          <div className="max-w-[800px] mx-auto mt-4 sm:mt-12">
+            {/*
+              메인 채팅 컴포넌트
+              messages: 메시지 목록
+              loading: 메시지 전송 중인지 여부
+              onSendMessage: 메시지 전송 함수
+            */}
+            <Chat
+              messages={messages}
+              loading={loading}
+              onSendMessage={handleSend}
+            />
+            {/* 메시지 목록의 끝으로 스크롤하기 위해 참조하는 엘리먼트 */}
+            <div ref={messagesEndRef} />
+          </div>
+        </div>
+
+        <div className="flex h-[30px] sm:h-[50px] border-t border-neutral-300 py-2 px-8 items-center sm:justify-between justify-center"></div>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`${inter.className} mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p
-            className={`${inter.className} m-0 max-w-[30ch] text-sm opacity-50`}
-          >
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`${inter.className} mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p
-            className={`${inter.className} m-0 max-w-[30ch] text-sm opacity-50`}
-          >
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`${inter.className} mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p
-            className={`${inter.className} m-0 max-w-[30ch] text-sm opacity-50`}
-          >
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`${inter.className} mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p
-            className={`${inter.className} m-0 max-w-[30ch] text-sm opacity-50`}
-          >
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+    </>
+  );
 }
