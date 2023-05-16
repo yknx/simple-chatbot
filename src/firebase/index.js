4b393cbc8650615caf927a0338b5e51c2c70e19e
Yekyung Nah <nyk7535@snu.ac.kr>
2023-05-16 12:22:38 +0900

    Update index.js

--- src/firebase/index.js
+++ src/firebase/index.js
@@ -1,21 +1,19 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDimI4doDgsii0TEaPvhu2vxRfuzWg29RM",
-  authDomain: "simplechatbot-55afb.firebaseapp.com",
-  projectId: "simplechatbot-55afb",
-  storageBucket: "simplechatbot-55afb.appspot.com",
-  messagingSenderId: "310120267269",
-  appId: "1:310120267269:web:b6ddcbb18d223ca351a66c",
-  measurementId: "G-B2H479PGXQ"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+const db = getFirestore(app);
+const auth = getAuth(app);
+
+export { auth, db };
